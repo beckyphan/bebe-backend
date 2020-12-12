3f661c1760ea64a05a3fe8b149ae56e05cadccdc
beckyphan <beckyphan@live.com>
2020-12-11 17:49:00 -0800

    ran migrations, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_231731) do
+ActiveRecord::Schema.define(version: 2020_12_09_041500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bebes", force: :cascade do |t|
+    t.string "name"
+    t.date "birthdate"
+    t.string "kind"
+    t.text "bio"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bebes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
