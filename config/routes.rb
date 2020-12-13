f60ce63df5f0f0d9d10a9cf9a37c85a3c803bd95
beckyphan <beckyphan@live.com>
2020-12-12 21:30:51 -0800

    nested bebes routes under user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users
-      resources :bebes
+      resources :users do
+        resources :bebes
+      end 
     end
   end
 
