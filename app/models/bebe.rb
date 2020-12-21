d1a3f8c71a35d808f45f90e05c98815b34e4f72f
beckyphan <beckyphan@live.com>
2020-12-20 19:07:46 -0800

    added has_many days association to Bebe model

--- app/models/bebe.rb
+++ app/models/bebe.rb
@@ -1,5 +1,6 @@
 class Bebe < ApplicationRecord
   belongs_to :user
+  has_many :days, dependent: :destroy
   validates :name, presence: true
   validates :birthdate, presence: true
   validates :kind, inclusion: { in: %w(human plant pet),
