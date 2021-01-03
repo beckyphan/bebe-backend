1ac8d6ab74befb54874d81953441db723e3527de
beckyphan <beckyphan@live.com>
2021-01-03 11:12:43 -0800

    draft controller action types and private methods for trackings

--- app/controllers/api/v1/trackings_controller.rb
+++ app/controllers/api/v1/trackings_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::TrackingsController < ApplicationController
+
+  #index - fetch all tracking data for day
+
+  #create - create a new data point
+
+  #update - edit an existing data point
+
+  #delete - delete an existing data point
+
+  private
+
+  def set_day
+    @day = Day.find_by_id(params[:day_id])
+  end
+
+  def tracking_params
+    params.require(:tracking).permit(:info_type, :start_time, :end_time, :amount, :amount_unit, :note)
+  end
 end
