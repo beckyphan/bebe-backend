204ca468cbc4579e478cfad610d4f1cec2e6939d
beckyphan <beckyphan@live.com>
2021-01-03 03:32:05 -0800

    updated day validations to add scope to date uniqueness

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,6 @@
 class Day < ApplicationRecord
   belongs_to :bebe
   validates :date, presence: true
-  validates :date, uniqueness: true
+  validates :date, uniqueness: { scope: :bebe_id,
+    message: "track data daily per bebe" }
 end
